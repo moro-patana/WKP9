76bdfd2433b729a42399c57af86200d478567318
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-08-29 14:22:27 +0300

    fetched the data, created an html and sorted the data

--- scripts.js
+++ scripts.js
@@ -1,2 +1,38 @@
-const baseEndpoint = 'http://ghibliapi.herokuapp.com/film';
-const proxy = 'https://cors-anywhere.herokuapp.com/';
+const baseEndpoint = 'https://ghibliapi.herokuapp.com/films';
+const moviesEl = document.querySelector(`.movies`);
+async function fetchFilms(movie) {
+    const response = await fetch(`${baseEndpoint}`);
+    const data = await response.json();
+    data.sort(function(a, b) {
+        return b.rt_score - a.rt_score;
+    })
+    return data;
+}
+const handleSubmit = async movie => {
+    const movies = await fetchFilms(movie);
+    console.log(movies);
+    displayFilms(movies);
+}
+function displayFilms(movies) {
+    const html = movies.map(movie => {
+        return `
+           <div class="movie">
+              <div class="heading">
+                <h2>${movie.title}</h2>
+                <span class="release_date">${movie.release_date}</span>
+                <span class="score">${movie.rt_score}</span>
+                </div>
+                <p class="description">
+                 ${movie.description}
+                </p>
+                <ul class="more">
+                    <li>${movie.director}</li>
+                    <li>${movie.producer}</li>
+                </ul>
+           </div>
+        `;
+    });
+    moviesEl.innerHTML = html.join('');
+};
+
+handleSubmit();
